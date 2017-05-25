5951777c9aa9c516a972bfa5045278968b5cbd48
Besnik <besniksh95@gmail.com>
2017-05-25 14:28:55 +0200

    Welcome Form

--- FinkiWAR/Program.cs
+++ FinkiWAR/Program.cs
@@ -15,7 +15,7 @@ namespace FinkiWAR
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FinkiWAR());
+            Application.Run(new Welcome());
         }
     }
 }
---
+++ FinkiWAR/Welcome.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FinkiWAR
+{
+    public partial class Welcome : Form
+    {
+        private bool started = false;
+        public Welcome()
+        {
+            InitializeComponent();
+            lblAuthors.Text = "Authors: \nBesnik Shabani and Semir Sinani";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FinkiWAR game = new FinkiWAR();
+            game.ShowDialog();
+        }
+
+        private void Welcome_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
