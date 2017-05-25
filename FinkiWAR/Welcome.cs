c46f7921821ca90cac94cd974987a59569a4ef0f
Besnik <besniksh95@gmail.com>
2017-05-25 17:10:45 +0200

    Added last comments

--- FinkiWAR/Welcome.cs
+++ FinkiWAR/Welcome.cs
@@ -9,10 +9,20 @@ using System.Windows.Forms;
 
 namespace FinkiWAR
 {
+    /// <summary>
+    /// Форма која се појавува најпрво
+    /// </summary>
     public partial class Welcome_FinkiWAR : Form
     {
-        private bool started = false;
+
+        /// <summary>
+        /// Најмногу пукнати непријатели
+        /// </summary>
         public int highscore;
+
+        /// <summary>
+        /// Пукнати непријатели од последна игра
+        /// </summary>
         public int score;
         public Welcome_FinkiWAR()
         {
@@ -22,7 +32,13 @@ namespace FinkiWAR
             lblAuthors.Text = "Authors: \nBesnik Shabani and Semir Sinani";
             lblRecord.Text = string.Format("Highscore : {0}", highscore);
         }
-
+        /// <summary>
+        /// Клик на копчето Старт.
+        /// Почеток на играта
+        /// Се чека одговор од играта за Најмногу пукнати непријатели
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void button1_Click(object sender, EventArgs e)
         {
             FinkiWAR game = new FinkiWAR();
